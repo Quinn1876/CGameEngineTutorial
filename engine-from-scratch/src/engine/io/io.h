743deed3cbc9045cd69fe14197cfb5ab968dca7c
Quinn Hodges <qhodges0442@gmail.com>
2023-04-23 21:57:12 -0400

    Check in after Episode 3 - Drawing a green square using shaders

---
+++ engine-from-scratch/src/engine/io/io.h
@@ -0,0 +1,16 @@
+#ifndef IO_H
+#define IO_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct file {
+    char *data;
+    size_t len;
+    bool is_valid;
+} File;
+
+File io_file_read(const char* path);
+int io_file_write(void *buffer, size_t size, const char *path);
+
+#endif
